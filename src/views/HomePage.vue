233a29aaa9cb0a6b18d9df8f6ec5082b9f44b3b6
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 22:27:22 +0100

    style: [position] center image on Home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,18 @@
 <template>
-  <main class="text-center content-center pt-5">
-    <img
-      src="/src/assets/images/post-its.png"
-      alt="Post-its pinned"
-      class="w-1/4 h-1/4"
-    />
-    <RouterLink
-      :to="{ name: 'showAll' }"
-      class="rounded-lg bg-sky-500 hover:bg-sky-700 p-3 cursor-pointer text-white"
-      >Voir les notes</RouterLink
-    >
+  <main class="flex flex-col text-center justify-content-center pt-5">
+    <div>
+      <img
+        src="/src/assets/images/post-its.png"
+        alt="Post-its pinned"
+        class="w-1/3 h-1/3 m-auto"
+      />
+    </div>
+    <div>
+      <RouterLink
+        :to="{ name: 'showAll' }"
+        class="rounded-lg bg-sky-500 hover:bg-sky-700 p-3 cursor-pointer text-white"
+        >Voir les notes</RouterLink
+      >
+    </div>
   </main>
 </template>
