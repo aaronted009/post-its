d0d809e25849598d95239aef2b6b5db638a60901
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 11:38:36 +0100

    chore: [clean imports] removed unued imports

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterView } from "vue-router";
-import ShowAllPostIts from "./ShowAllPostIts.vue";
-import EditPostIt from "@/views/EditPostIt.vue";
 </script>
 
 <template>
