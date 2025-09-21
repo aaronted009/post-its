1a6a528e8f7f2e5c1838c909e970f6ae3ea5cecc
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 16:28:55 +0100

    style: [add margin] add margin bottom to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="p-5 shadow-sm shadow-gray-400">
+  <section class="p-5 shadow-sm shadow-gray-400 mb-5">
     <div class="flex font-bold text-3xl">
       <div>📑</div>
       <div>Mes notes</div>
