e5710f5c167fbf293286910510e024633be5cdc1
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 22:31:44 +0100

    feat: [validation] prevent a note to be recorded empty

--- src/views/CreatePostIt.vue
+++ src/views/CreatePostIt.vue
@@ -15,12 +15,30 @@ onMounted(() => {
   <main class="w-full px-5">
     <form class="bg-green-100 p-5 rounded m-auto max-w-1/2">
       <label for="title" class="block text-sm/6 font-medium">Titre</label>
-      <input class="border rounded" type="text" name="title" id="title" v-model="note.postItTitle" />
+      <input
+        class="border rounded"
+        type="text"
+        name="title"
+        id="title"
+        required
+        v-model="note.postItTitle"
+      />
       <br />
       <label for="content" class="block text-sm/6 font-medium">Contenu</label>
-      <textarea class="border rounded" name="content" id="content" v-model="note.postItContent"></textarea>
+      <textarea
+        class="border rounded"
+        name="content"
+        id="content"
+        required
+        v-model="note.postItContent"
+      ></textarea>
       <br />
-      <input @click="note.create" type="button" value="Créer" class="rounded shadow-sm shadow-gray-400 cursor-pointer mt-5 px-5 py-2 bg-green-300" />
+      <input
+        @click="note.create"
+        type="submit"
+        value="Créer"
+        class="rounded shadow-sm shadow-gray-400 cursor-pointer mt-5 px-5 py-2 bg-green-300"
+      />
     </form>
   </main>
 </template>
