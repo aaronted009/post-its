f19ad133eddba6ccd7373c005a457de157f26eea
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 22:20:28 +0100

    style: [style] update CreatePostIt view style

--- src/views/CreatePostIt.vue
+++ src/views/CreatePostIt.vue
@@ -12,15 +12,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="w-full mx-5">
-    <form class="bg-green-100 p-5 rounded">
+  <main class="w-full px-5">
+    <form class="bg-green-100 p-5 rounded m-auto max-w-1/2">
       <label for="title" class="block text-sm/6 font-medium">Titre</label>
       <input class="border rounded" type="text" name="title" id="title" v-model="note.postItTitle" />
       <br />
       <label for="content" class="block text-sm/6 font-medium">Contenu</label>
       <textarea class="border rounded" name="content" id="content" v-model="note.postItContent"></textarea>
       <br />
-      <input @click="note.create" type="button" value="Créer" class="border rounded cursor-pointer mt-5 px-5 py-2 bg-green-300" />
+      <input @click="note.create" type="button" value="Créer" class="rounded shadow-sm shadow-gray-400 cursor-pointer mt-5 px-5 py-2 bg-green-300" />
     </form>
   </main>
 </template>
