f0950d3fc32e0ff9973a0faa2d8432086b788940
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 15:27:26 +0100

    feat: [add components] add Header & Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+</script>
+
+<template>
+  <footer
+    class="fixed bottom-0 w-full text-center py-5 bg-white shadow-sm shadow-gray-400 text-gray-500"
+  >
+    © {{ currentYear }} Made by
+    <a href="https://github.com/aaronted009/">Teddy Aaron ABALLO</a>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="p-5 shadow-sm shadow-gray-400">
+    <div class="flex font-bold text-3xl">
+      <div>📑</div>
+      <div>Mes notes</div>
+    </div>
+    <div>Organisez vos idées, simplement et efficacement</div>
+  </section>
+</template>
