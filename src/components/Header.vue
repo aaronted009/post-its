be3a28de4b5bdf4af720f014aef38329f7a0134f
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 21:31:39 +0100

    feat(navigation) [user experience] add link to Header to go back to home page in order to enhance smooth navigation in the app

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="p-5 shadow-sm shadow-gray-400 mb-5">
-    <div class="flex font-bold text-3xl">
-      <div>📑</div>
-      <div>Mes notes</div>
-    </div>
-    <div>Organisez vos idées, simplement et efficacement</div>
+    <RouterLink :to="{ name: 'home' }">
+      <div class="flex font-bold text-3xl">
+        <div>📑</div>
+        <div>Mes notes</div>
+      </div>
+      <div>Organisez vos idées, simplement et efficacement</div>
+    </RouterLink>
   </section>
 </template>
