7d157d1bc6f4d22bd8bab94a7081d28cc207ba6b
Teddy Aballo <teddy.aballo@epitech.eu>
2025-09-21 17:38:39 +0100

    style [footer style] update footer style by setting position to relative and add a margin-top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@ let currentYear = currentDate.getFullYear();
 
 <template>
   <footer
-    class="fixed bottom-0 w-full text-center py-5 bg-white shadow-sm shadow-gray-400 text-gray-500"
+    class="relative bottom-0 w-full text-center py-5 bg-white shadow-sm shadow-gray-400 text-gray-500 mt-5"
   >
     © {{ currentYear }} Made by
     <a href="https://github.com/aaronted009/">Teddy Aaron ABALLO</a>
